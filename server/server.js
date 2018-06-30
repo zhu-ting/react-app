dabcdb236e5ea375eb7b7bf8944816d2220586ea
Judy Zhu <850832627@qq.com>
2018-06-30 17:02:35 +0800

    can not destructure  property username

--- server/server.js
+++ server/server.js
@@ -6,9 +6,13 @@ const userRouter = require('./user')
 const app = express()
 app.use(cookieParser())
 app.use(bodyParser.json())
+
+app.get('/', function(req, res){
+	return res.json({code: 35})
+})
 app.use('/user', userRouter)
-app.listen(9077,function(){
-	console.log('Node app start at port 9077')
+app.listen(7070,function(){
+	console.log('Node app start at port 7070')
 })
 
 
