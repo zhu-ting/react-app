a5d0036be84693fb08707b2e6e30669ca6d04a9d
Judy Zhu <850832627@qq.com>
2018-07-06 00:38:38 +0800

    RadioItem must be wrapped in List

--- src/container/Register.js
+++ src/container/Register.js
@@ -24,7 +24,68 @@ class Register extends Component {
     })
   }
 
-  handleRegister(){
+  handleRegister(){import React, { Component } from 'react';
+  import { List, InputItem, Button, Radio } from 'antd-mobile';
+  import App from '../App';
+  import { connect } from 'react-redux';
+  import { register } from '../redux/user.redux';
+  import { Redirect } from 'react-router-dom'
+  @connect(
+    state => state.user,
+    { register }
+  )
+  
+  class Register extends Component {
+    constructor(props) {
+      super(props);
+      this.handleRegister = this.handleRegister.bind(this)
+      this.handleChange = this.handleChange.bind(this)
+      this.state = {
+        user: '',
+        pwd: '',
+        type: 'genius'
+      }
+    }
+  
+    handleChange(key, value) {
+      console.log(this.state)
+      this.setState({
+        [key]: value
+      })
+    }
+  
+    handleRegister() {
+      
+      this.props.register(this.state)
+    }
+  
+    render() {
+      const RadioItem = Radio.RadioItem
+      return (
+        <div className="App">
+        {this.props.redirectTo? <Redirect to={this.props.redirectTo}></Redirect>:null}
+          <App />
+          <InputItem onChange={v => this.handleChange('user', v)}>
+            Username
+         </InputItem>
+          <InputItem onChange={v => this.handleChange('pwd', v)} type='password'>
+            Password
+         </InputItem>
+         <List>
+         <RadioItem checked={this.state.type === 'boss'} onChange={() => this.handleChange('type', 'boss')}>
+            boss
+          </RadioItem>
+          <RadioItem checked={this.state.type === 'genius'} onChange={() => this.handleChange('type', 'genius')}>
+            genius
+          </RadioItem>
+           </List>
+          <Button onClick={() => this.handleRegister()}>Sign up</Button>
+        </div>
+      );
+    }
+  }
+  
+  export default Register;
     this.props.register(this.state)
   }
 
