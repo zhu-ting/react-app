62c2e4ff62540f607a378af274165b0e4baeb5ab
Judy Zhu <850832627@qq.com>
2018-07-07 17:29:28 +0800

    delete

--- src/container/Register.js
+++ src/container/Register.js
@@ -1,30 +1,4 @@
-import React, { Component } from 'react';
-import { InputItem, Button } from 'antd-mobile';
-import { connect } from 'react-redux';
-import { register } from '../redux/user.redux';
-import App from '../App';
-
-@connect(
-  state => state.user,
-  {register}
-)
-
-class Register extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      username: '',
-      password: '',
-    }
-  }
-
-  handleChange(key, value){
-    this.setState({
-      [key]: value
-    })
-  }
-
-  handleRegister(){import React, { Component } from 'react';
+  import React, { Component } from 'react'
   import { List, InputItem, Button, Radio } from 'antd-mobile';
   import App from '../App';
   import { connect } from 'react-redux';
@@ -85,24 +59,4 @@ class Register extends Component {
     }
   }
   
-  export default Register;
-    this.props.register(this.state)
-  }
-
-  render() {
-    return (
-      <div className="App">
-       <App />
-       <InputItem onChange={v => this.handleChange('username', v)}>
-       Username
-       </InputItem>
-       <InputItem onChange={v => this.handleChange('password', v)}>
-       Password
-       </InputItem>
-       <Button onClick={() => this.handleRegister()}>Sign up</Button>
-      </div>
-    );
-  }
-}
-
-export default Register;
+export default Register
