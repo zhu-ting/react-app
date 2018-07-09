2cd44466f54694dab554c2cd66319e84c56e7053
Judy Zhu <850832627@qq.com>
2018-07-09 22:01:43 +0800

    pay attention to the switch

--- src/index.js
+++ src/index.js
@@ -30,14 +30,15 @@ ReactDOM.render(
   <Provider store={store}>
       <BrowserRouter>
         <div>
-          {/* <Switch> */}
+          
             <AuthRoute></AuthRoute>
-            <Route path="/login" exact component={Login}></Route>
+            <Switch>
+            <Route path="/login" component={Login}></Route>
             <Route path="/register" component={Register}></Route>
             <Route path="/bossinfo" component={BossInfo}></Route>
             <Route path="/geniusinfo" component={GeniusInfo}></Route>
             <Route component={Dashboard}></Route>
-          {/* </Switch> */}
+          </Switch>
         </div>
       </BrowserRouter>
   </Provider>
