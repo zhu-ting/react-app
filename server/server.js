8b1b03576a06c645a334fcd5b00f7f2c59e2b791
Judy Zhu <850832627@qq.com>
2018-07-13 23:08:17 +0800

    integration test

--- server/server.js
+++ server/server.js
@@ -2,13 +2,27 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 
+const app = express()
+// work with express
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+// io is global while socket is the current one
+io.on('connection', function(socket){
+	console.log('user login')
+    socket.on('sendmsg', function(data){
+		console.log(data)
+		io.emit('recevmsg', data)
+	})
+})
+
 const userRouter = require('./user')
 
-const app = express()
+
 app.use(cookieParser())
 app.use(bodyParser.json())
 app.use('/user',userRouter)
-app.listen(7000,function(){
+server.listen(7000,function(){
 	console.log('Node app start at port 7000')
 })
 
--- src/component/Chat.js
+++ src/component/Chat.js
@@ -1,9 +1,47 @@
 import React from 'react';
+import io from 'socket.io-client';
+import { List, InputItem } from 'antd-mobile';
+import Input from 'antd-mobile/lib/input-item/Input';
+const socket = io('ws://localhost:7000')
 
 class Chat extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            text: '',
+            msg: []
+        }
+    }
+
+    componentDidMount(){
+       socket.on('recvmsg', (data) => {
+           console.log(data)
+           this.setState({
+               msg: [...this.state.msg, data.text]
+           })
+       })
+    }
+
+    handleSubmit(){
+        socket.emit('sendmsg', {text: this.state.text})
+        this.setState({text: ''})
+    }
    render(){
        return (
-           <h2>chat with user: {this.props.match.params.user}</h2>
+           <div>
+               {this.state.msg.map(v => {
+                   return <p key={v}>{v}</p>
+               })}
+               <List>
+                   <InputItem
+                     placeholder='please input'
+                     value={this.state.text}
+                     onChange={v=>{this.setState({text:v})}}
+                     extra={<span onClick={() => this.handleSubmit}>Submit</span>}
+                  >
+                  </InputItem>
+               </List>
+           </div>
        )
    }
 }
