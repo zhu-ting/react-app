f94c6115ad0e9e83b78760946c9b8bf7f0fea2eb
Judy Zhu <850832627@qq.com>
2018-07-16 00:14:34 +0800

    chat

--- src/component/Chat.js
+++ src/component/Chat.js
@@ -1,49 +1,143 @@
-import React from 'react';
-import io from 'socket.io-client';
-import { List, InputItem } from 'antd-mobile';
-import Input from 'antd-mobile/lib/input-item/Input';
+import React from 'react'
+import {List,InputItem,NavBar,Icon, Grid} from 'antd-mobile'
+import io from 'socket.io-client'
+import {connect} from 'react-redux'
+import {getMsgList, sendMsg, recvMsg, readMsg} from '../../redux/chat.redux'
+import {getChatId} from '../../util'
 const socket = io('ws://localhost:7000')
 
+
+// $('#test').find('img')
+// $('#test img')
+
+@connect(
+	state=>state,
+	{ getMsgList, sendMsg, recvMsg, readMsg}
+)
 class Chat extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            text: '',
-            msg: []
-        }
-    }
+	constructor(props){
+		super(props)
+		this.state = {text:'',msg:[]}
+	}
+	componentDidMount(){
+		if (!this.props.chat.chatmsg.length) {
+			this.props.getMsgList()
+			this.props.recvMsg()	
+		}
+	}
+	componentWillUnmount(){
+		const to = this.props.match.params.user
+		this.props.readMsg(to)
+	}
+	fixCarousel(){
+		setTimeout(function(){
+			window.dispatchEvent(new Event('resize'))
+		},0)
+	}
+	handleSubmit(){
+		// socket.emit('sendmsg',{text:this.state.text})
+		// this.setState({text:''})
+		const from = this.props.user._id
+		const to = this.props.match.params.user
+		const msg = this.state.text
+		this.props.sendMsg({from,to,msg})
+		this.setState({
+			text:'',
+			showEmoji:false
+		})
+	}
+	render(){
+		const emoji = '😀 😃 😄 😁 😆 😅 😂 😊 😇 🙂 🙃 😉 😌 😍 😘 😗 😙 😚 😋 😜 😝 😛 🤑 🤗 🤓 😎 😏 😒 😞 😔 😟 😕 🙁 😣 😖 😫 😩 😤 😠 😡 😶 😐 😑 😯 😦 😧 😮 😲 😵 😳 😱 😨 😰 😢 😥 😭 😓 😪 😴 🙄 🤔 😬 🤐 😷 🤒 🤕 😈 👿 👹 👺 💩 👻 💀 ☠️ 👽 👾 🤖 🎃 😺 😸 😹 😻 😼 😽 🙀 😿 😾 👐 🙌 👏 🙏 👍 👎 👊 ✊ 🤘 👌 👈 👉 👆 👇 ✋  🖐 🖖 👋  💪 🖕 ✍️  💅 🖖 💄 💋 👄 👅 👂 👃 👁 👀 '
+										.split(' ')
+										.filter(v=>v)
+										.map(v=>({text:v}))
 
-    componentDidMount(){
-       socket.on('recvmsg', (data) => {
-           console.log(data)
-           this.setState({
-               msg: [...this.state.msg, data.text]
-           })
-       })
-    }
+		const userid = this.props.match.params.user
+		const Item = List.Item
+		const users = this.props.chat.users
+		if(!users[userid]){
+			return null
+		}
+		const chatid = getChatId(userid, this.props.user._id)
+		const chatmsgs = this.props.chat.chatmsg.filter(v=>v.chatid==chatid)
+		return (
+			<div id='chat-page'>
+				<NavBar
+					mode='dark'
+					icon={<Icon type="left" />}
+					onLeftClick={()=>{
+						this.props.history.goBack()
+					}}
+				>
+					{users[userid].name}
+				</NavBar>
 
-    handleSubmit(){
-        socket.emit('sendmsg', {text: this.state.text})
-        this.setState({text: ''})
-    }
-   render(){
-       return (
-           <div>
-               {this.state.msg.map(v => {
-                   return <p key={v}>{v}</p>
-               })}
-               <List>
-                   <InputItem
-                     placeholder='please input'
-                     value={this.state.text}
-                     onChange={v=>{this.setState({text:v})}}
-                     extra={<span onClick={() => this.handleSubmit}>Submit</span>}
-                  >
-                  </InputItem>
-               </List>
-           </div>
-       )
-   }
+				
+				{chatmsgs.map(v=>{
+					const avatar = require(`../img/${users[v.from].avatar}.png`)
+					return v.from==userid?(
+						<List key={v._id}>
+							<Item
+								thumb={avatar}
+							>{v.content}</Item>
+						</List>
+					
+					):(
+						<List key={v._id}>
+							<Item
+								extra={<img src={avatar} />}
+							 	className='chat-me'
+							 	>{v.content}</Item>
+						</List>
+
+					)
+					
+				})}
+			
+				<div className="stick-footer">
+					
+					<List>
+						<InputItem
+							placeholder='please input'
+							value={this.state.text}
+							onChange={v=>{
+								this.setState({text:v})
+							}}
+							extra={
+
+								<div>
+									<span
+										style={{marginRight:15}}
+										onClick={()=>{
+											this.setState({
+												showEmoji:!this.state.showEmoji
+											})
+											this.fixCarousel()
+										}}
+									>😃</span>
+									<span onClick={()=>this.handleSubmit()} >Submit</span>
+								</div>
+							}
+						></InputItem>
+					</List>
+
+					{this.state.showEmoji?<Grid 
+						data={emoji}
+						columnNum={9}
+						carouselMaxRow={4}
+						isCarousel={true}
+						onClick={el=>{
+							this.setState({
+								text:this.state.text+el.text
+							})
+							
+						}}
+					/>:null}
+					
+				</div>
+			</div>
+		)
+	}
 }
 
-export default Chat;
+export default Chat
